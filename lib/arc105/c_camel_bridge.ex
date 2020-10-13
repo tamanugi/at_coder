f2576fce618785e39265e02031d3bd38b619d2f1
tamanugi <tamanugi@gmail.com>
2020-10-13 23:34:13 +0900

    c.camel_bridge ac

--- lib/arc105/c_camel_bridge.ex
+++ lib/arc105/c_camel_bridge.ex
@@ -1,7 +1,7 @@
 defmodule CamelBridge.Main do
   def main() do
     [n, m] = read_array()
-    ws = read_array()
+    [ws_h | ws_t] = read_array() |> Enum.sort(:desc)
     lvs =
       IO.read(:all)
       |> String.split("\n")
@@ -10,30 +10,40 @@ defmodule CamelBridge.Main do
       |> Enum.sort_by(fn [_, v] -> v end)
       |> read_map(0, %{})
 
-    # {:ok, ans} = start_link(1000_000_000)
+    # IO.inspect({n, m})
+    # IO.inspect([ws_h | ws_t])
+    # IO.inspect(lvs)
 
-    IO.inspect({n, m})
-    IO.inspect(ws)
-    IO.inspect(lvs)
+    with [_, v] <- lvs[0],
+      true <- v >= ws_h
+    do
+      # LVSのlを調整する
+      lvs =
+        if m > 1 do
+          1..(m-1)
+          |> Enum.reduce(lvs, fn i, acc ->
+            with [l1, _] <- acc[i - 1]
+            do
+              Map.update!(acc, i, fn [l, v] -> [max(l, l1), v] end)
+            end
+          end)
+        else
+          lvs
+        end
 
-    # LVSのlを調整する
-    lvs = 1..(m-1)
-    |> Enum.reduce(lvs, fn i, acc ->
-      with [l1, _] <- acc[i - 1]
-      do
-        Map.update!(acc, i, fn [l, v] -> [max(l, l1), v] end)
-      end
-    end)
-    IO.inspect(lvs)
-
-    permutation(ws, n)
-    |> solve(lvs, n, m, 1000_000_000)
-    |> IO.inspect()
+      # 大きさが最大のラクダは端固定できる
+      permutation(ws_t, n - 1)
+      |> Enum.map(fn ws -> [ws_h | ws] end)
+      |> Enum.map(fn ws -> Enum.reverse(ws) end)
+      |> solve(lvs, n, m, 1000_000_000)
+      |> IO.puts()
+    else
+      _ -> IO.puts(-1)
+    end
   end
 
   def solve([], _, _, _, ans), do: ans
   def solve([ws | tail], lvs, n, m, ans) do
-    # ws_map = ws |> read_map(0, %{})
     ds =  process(0, %{}, ws, lvs, n, m)
     dsv = Map.get(ds, n - 1)
     ans = min(dsv, ans)
@@ -51,10 +61,8 @@ defmodule CamelBridge.Main do
   def process2(w, ds, i, j, ws, lvs, n, m) when j <= n - 1 do
     w = w + Enum.at(ws, j)
 
-    # TODO: 二分探索
-    k = lvs |> Map.values |>  Enum.find_index(fn [_, v] -> v >= w end) || m
-    IO.inspect(if k > 0, do: lvs[k - 1] |> List.first(), else: 0)
-    # k = m
+    # 二分探索
+    k = binary_search_vw(lvs, w) || m
 
     dsj = Map.get(ds, j, 0)
     dsi = Map.get(ds, i, 0) + if k > 0, do: lvs[k - 1] |> List.first(), else: 0
@@ -64,8 +72,30 @@ defmodule CamelBridge.Main do
   end
   def process2(_, ds, _, _, _, _, _, _), do: ds
 
-  def binary_search_vw(lvs, w, len) do
-    div(len, 2)
+  def binary_search_vw(lvs, w), do: binary_search_vw(lvs, w, 0, map_size(lvs))
+  def binary_search_vw(lvs, w, left, right) when right - left == 1 do
+    case {lvs[left], lvs[right]} do
+      {[_, ^w], _} -> left
+      {_, [_, ^w]} -> right
+      {[_, _], nil} -> nil
+      {nil, [_, _]} -> nil
+      {[_, vl], [_, vr]} ->
+        cond do
+          vl > w -> left
+          vr >= w -> right
+        end
+    end
+  end
+  def binary_search_vw(lvs, w, left, right) do
+    mid = div(right - left, 2) + left
+
+    with [_, v] <- Map.get(lvs, mid),
+          true <- v < w
+    do
+      binary_search_vw(lvs, w, mid, right)
+    else
+      _ -> binary_search_vw(lvs, w, left, mid)
+    end
 
   end
 
