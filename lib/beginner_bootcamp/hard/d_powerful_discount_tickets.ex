5bc102bff959ce817e6ec8274e17548a2525f14a
tamanugi <tamanugi@gmail.com>
2020-11-01 14:33:52 +0900

    d powerful discount tickets AC

--- lib/beginner_bootcamp/hard/d_powerful_discount_tickets.ex
+++ lib/beginner_bootcamp/hard/d_powerful_discount_tickets.ex
@@ -7,34 +7,33 @@ defmodule PowerfulDiscountTickets.Main do
     [_, m] = read_array()
     as = read_array() |> Enum.sort(:desc)
 
-    solve(as, [], m)
+    solve(as, m)
     |> IO.puts()
   end
 
-  def solve(as, acc, 0), do: Enum.sum(as) + Enum.sum(acc)
-  def solve([h | t], [], m), do: solve(t, [div(h, 2)], m - 1)
-  def solve([], [qh | qt], m), do: solve([], append_tail(qt, div(qh, 2))  , m - 1)
-  def solve([h | t] = as, [qh | qt] = acc, m) do
-
-    {as, acc} =
+  def solve([h | t], m), do: solve(t, {[], [div(h, 2)]}, m - 1)
+  def solve(as, {l, r}, 0), do: Enum.sum(as) + Enum.sum(l) + Enum.sum(r)
+  def solve([], q, m) do
+    {{:value, qh}, rest} = :queue.out(q)
+    solve([], :queue.in(div(qh, 2), rest)   , m - 1)
+  end
+  def solve([h | t] = as, q, m) do
+    {{:value, qh}, rest} = :queue.out(q)
+    {as, q} =
       if qh > h do
-        {as, append_tail(qt, div(qh, 2))}
+        {as, :queue.in(div(qh, 2), rest)}
       else
-        {t, append_tail(acc, div(h, 2))}
+        q = :queue.in(div(h, 2), rest)
+        q = :queue.in_r(qh, q)
+        {t, q}
       end
 
-    solve(as, acc, m - 1)
-  end
-
-  def append_tail(list, element) do
-    # This functions is bottleneck....
-    # list ++ [element]
-    [element | Enum.reverse(list)] |> Enum.reverse()
+    solve(as, q, m - 1)
   end
 
   def test_case do
     as = for i <- 10000..1, do: i
-    {runtime, result} =  :timer.tc(&solve/3, [as, [], 100000])
+    {runtime, result} =  :timer.tc(&solve/2, [as, 100000])
 
     # We have to make sure the run time is less than 2 seconds.
     IO.inspect([runtime: runtime, result: runtime < 2000_000])
