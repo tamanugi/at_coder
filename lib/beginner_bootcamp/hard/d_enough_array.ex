9bbd3a51a9d7ed6fc1440d9a6dca14819e26a1c8
tamanugi <tamanugi@gmail.com>
2020-11-01 20:57:42 +0900

    d enough array wip

--- lib/beginner_bootcamp/hard/d_enough_array.ex
+++ lib/beginner_bootcamp/hard/d_enough_array.ex
@@ -4,40 +4,53 @@ defmodule EnoughArray.Main do
   end
 
   def main() do
-    [_, k] = read_array()
+    [n, k] = read_array()
 
     as = read_array()
-    sum = Enum.sum(as)
 
-    cond do
-      sum > k -> solve(as, sum, k, 0)
-      sum == k -> 1
-      sum < k -> 0
-    end
+    solve(as, k, n)
     |> IO.puts()
   end
 
-  def solve([], _sum, _k, acc), do: acc
-  def solve(_, sum, k, acc) when sum < k, do: acc
-  def solve([h | t] = as, sum, k, acc) do
-    acc = acc + process(Enum.reverse(as), sum, k, 0)
-    sum = sum - h
-    solve(t, sum, k, acc)
+  def solve(as, k, n) do
+    as
+    |> Enum.reduce([0], fn a, [h | _] = acc ->
+      [a + h | acc]
+    end)
+    |> Enum.reverse()
+    |> Enum.with_index()
+    |> solve(k, n, 0)
   end
 
-  def process([], _sum, _k, acc), do: acc
-  def process(_, sum, k, acc) when sum < k, do: acc
-  def process([h | t], sum, k, acc) do
-    acc = if sum >= k, do: acc + 1, else: acc
-    sum = sum - h
-    process(t, sum, k, acc)
+  def solve([], _k, _n, acc), do: acc
+  def solve([{h, _} | t] = as, k, n, acc) do
+    IO.inspect(h)
+    r =
+      t
+      |> Enum.reduce_while(n, fn {sum, i}, _ ->
+        if sum - h > k do
+          {:halt, i}
+        else
+          {:cont, n}
+        end
+      end)
+
+    if r < n do
+      solve(t, k, n, acc + (n - r  + 1))
+    else
+      solve(t, k, n, acc)
+    end
   end
+
+  def process([{h, i} | _], a, k) when (h - a) >= k, do: i
+  def process([_ | t], a, k), do: process(t, a, k)
+  def process([], _, _), do: nil
 
   def test() do
     n = 100000
     as = for _ <- 1..n, do: floor(:rand.uniform() * n)
-    k = 1000
-    :timer.tc(&solve/4, [as, Enum.sum(as), k, 0])
+    k = 100000 - 10
+    :timer.tc(&solve/3, [as, k, n])
     |> IO.inspect()
   end
 
