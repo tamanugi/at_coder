2450eb469a2a8cdcb679b015c8c343678110bef8
tamanugi <tamanugi@gmail.com>
2020-11-01 20:57:42 +0900

    d enough array AC

--- lib/beginner_bootcamp/hard/d_enough_array.ex
+++ lib/beginner_bootcamp/hard/d_enough_array.ex
@@ -3,41 +3,56 @@ defmodule EnoughArray.Main do
     IO.read(:line) |> String.trim() |> String.split(" ") |> Enum.map(&String.to_integer/1)
   end
 
+  def read_map() do
+    IO.read(:line)
+    |> String.trim()
+    |> String.split(" ")
+    |> read_map(0, %{})
+  end
+
+  def read_map([], _, acc), do: acc
+  def read_map([h|t], i, acc) do
+    acc = Map.put(acc, i, String.to_integer(h))
+    read_map(t, i + 1, acc)
+  end
+
   def main() do
-    [_, k] = read_array()
+    [n, k] = read_array()
 
-    as = read_array()
-    sum = Enum.sum(as)
+    as = read_map()
 
-    cond do
-      sum > k -> solve(as, sum, k, 0)
-      sum == k -> 1
-      sum < k -> 0
-    end
+    solve(as, n, k)
     |> IO.puts()
   end
 
-  def solve([], _sum, _k, acc), do: acc
-  def solve(_, sum, k, acc) when sum < k, do: acc
-  def solve([h | t] = as, sum, k, acc) do
-    acc = acc + process(Enum.reverse(as), sum, k, 0)
-    sum = sum - h
-    solve(t, sum, k, acc)
+  def solve(as, n, k) do
+    solve({0, 0, 0}, as, n, k, 0)
   end
 
-  def process([], _sum, _k, acc), do: acc
-  def process(_, sum, k, acc) when sum < k, do: acc
-  def process([h | t], sum, k, acc) do
-    acc = if sum >= k, do: acc + 1, else: acc
+  def solve({l, _, _}, _, n, _, acc) when l == n, do: acc
+  def solve({l, r, sum}, as, n, k, acc) do
+    h = as[l]
+    {r, sum} = find_r({r, sum}, as, n, k)
+    acc =  if sum >= k, do: acc + n - r + 1, else: acc
     sum = sum - h
-    process(t, sum, k, acc)
+    solve({l + 1, r, sum}, as, n, k, acc)
+  end
+
+  def find_r({r, sum}, _, n, _) when r == n, do: {n, sum}
+  def find_r({r, sum}, as, n, k) do
+    if sum >= k do
+      {r, sum}
+    else
+      sum = sum + as[r]
+      find_r({r + 1, sum}, as, n, k)
+    end
   end
 
   def test() do
     n = 100000
     as = for _ <- 1..n, do: floor(:rand.uniform() * n)
-    k = 1000
-    :timer.tc(&solve/4, [as, Enum.sum(as), k, 0])
+    k = 100000 - 10
+    :timer.tc(&solve/3, [as, k, n])
     |> IO.inspect()
   end
 
