957b9ac68ca512888babe5b4397de156fdac613a
tamanugi <tamanugi@gmail.com>
2020-11-10 20:46:00 +0900

    d wandering AC

--- lib/abc182/d_wandering.ex
+++ lib/abc182/d_wandering.ex
@@ -11,52 +11,34 @@ defmodule Wandering.Main do
     n = read_single()
     as = read_array()
 
-    m = as |> Enum.with_index(1) |> Enum.map(fn {v, i} -> {i, v} end) |> Map.new()
+    # 各iでの座標移動値
+    # 各iでの座標の最大値
+    {pmap, qmap} = p(as)
 
-    # それぞれ時点での累積和
-    sarray = sum_array(as, [])
 
-    {c, idx} = sarray |> Enum.max_by(fn {v, _} -> v end)
+    {_, r} =
+      1..n
+      |> Enum.reduce({0, 0}, fn i, {x, r} ->
+        r = max(r, x + qmap[i])
+        x = x + pmap[i]
+        {x, r}
+      end)
 
-    smap = sarray |> Enum.map(fn {v, i} -> {i, v} end) |> Map.new()
-
-    unless c <= 0 do
-      # idx　で止めたとき
-      res1 =
-        1..idx
-        |> Enum.reduce(0, fn i, acc ->
-           acc + (idx + 1 - i) * m[i]
-        end)
-
-      # もう一週したとき
-      res2 =
-        res1 + m[idx+1] + smap[idx]
-
-      if res1 >= res2 do
-        res1
-      else
-        sss = smap[idx]
-        {_, stop} =  sarray |> Enum.find({0, n}, fn {v, i} -> v < -1 * sss and i > idx end)
-
-        # 回れるだけ回ったほうがいい
-        1..stop
-        |> Enum.reduce(0, fn i, acc ->
-          if i <= idx do
-            acc + (stop + 1 - i) * m[i]
-          else
-            # 最後の週のidxより後は進まない
-            acc + (stop - i) * m[i]
-          end
-        end)
-      end
-    else
-      0
-    end
+    r
     |> IO.puts()
+
   end
 
-  def sum_array([], acc), do: acc |> Enum.reverse() |> Enum.with_index(1)
-  def sum_array([h | t], []), do: sum_array(t, [h])
-  def sum_array([h | t], [p | _] = acc), do: sum_array(t, [p + h | acc])
+  def p([h | t]), do: p(t, [h], [h])
+
+  def p([], acc, acc2) do
+    {
+     acc |> Enum.reverse() |> Enum.with_index(1) |> Enum.map(fn {v, i} -> {i, v} end) |> Map.new(),
+     acc2 |> Enum.reverse() |> Enum.with_index(1) |> Enum.map(fn {v, i} -> {i, v} end) |> Map.new()
+    }
+  end
+  def p([h | t], [p | _] = acc, [p2 | _] = acc2) do
+    p(t, [p + h | acc], [ max(p + h, p2) | acc2])
+  end
 
 end
