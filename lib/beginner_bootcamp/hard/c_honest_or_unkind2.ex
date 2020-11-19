0221e5dcfb0749dc8d0af5d17534f0ecd0cb83f7
tamanugi <tamanugi@gmail.com>
2020-11-19 18:48:23 +0900

    c honest or unkind2 AC

--- lib/beginner_bootcamp/hard/c_honest_or_unkind2.ex
+++ lib/beginner_bootcamp/hard/c_honest_or_unkind2.ex
@@ -1,4 +1,6 @@
 defmodule HonestOrUnkind2.Main do
+  use Bitwise
+
   def read_single() do
     IO.read(:line) |> String.trim() |> String.to_integer()
   end
@@ -10,77 +12,62 @@ defmodule HonestOrUnkind2.Main do
   def main() do
     n = read_single()
 
-    testimony = 1..n+1 |> testimony(%{}) |> IO.inspect()
+    testimony = 1..n+1 |> testimony(%{})
 
-    testimony
-    |> Map.values()
-    |> List.flatten()
-    |> Enum.chunk_every(2)
-    |> Enum.reduce(%{}, fn [i, s], acc ->
-      Map.update(acc, i, [s], fn ss -> [s | ss] end)
-    end)
-    |> IO.inspect()
-
-    1..n+1
+    # bit全探索
+    1..(1 <<< n)
     |> solve(testimony, 0)
     |> IO.puts()
   end
 
   def solve(n..n, _, acc), do: acc
   def solve(i..n, testimony, acc) do
-    IO.puts("if #{i} is honest")
+
+    honests =
+      i
+      # bitにする
+      |> Integer.digits(2)
+      # bitから復元
+      |> Enum.reverse()
+      |> Enum.with_index()
+      |> Enum.filter(fn {v, _} -> v == 1 end)
+      |> Enum.map(fn {_, i} -> i + 1 end)
 
     acc =
-      if Map.has_key?(testimony, i) do
-        case judge([i], testimony, %{i => 1}) do
-          0 -> acc
-          stats ->
-            IO.inspect(stats)
-            1..n-1
-            |> Enum.map(fn j -> Map.get(stats, j, 0) end)
-            |> Enum.filter(&(&1 == 1))
-            |> Enum.sum()
-            |> IO.inspect()
-            |> max(acc)
-        end
-      else
-        acc
+      case judge(honests, testimony) do
+        :ok -> Enum.count(honests)
+        :ng -> 0
       end
+      |> max(acc)
 
     solve(i+1..n, testimony, acc)
   end
 
-  def judge([], _, stats), do: stats
+  def judge(honests, testimony) do
+    stats =
+      for i <- honests, into: %{}, do: {i, 1}
+
+    judge(honests, testimony, stats)
+  end
+  def judge([], _, _), do: :ok
   def judge([h | t], testimony, stats) do
-    s = stats[h]
-    IO.inspect(stats)
+    res =
+      Map.get(testimony, h, [])
+      |> Enum.reduce_while(:ok, fn [person, hu], _ ->
+        case Map.get(stats, person, 0) do
+          ^hu ->
+            # 証言と一致
+            {:cont, :ok}
 
-    if s == 1 do
-      # 正直者の証言は信用する
-      res =
-        Map.get(testimony, h, [])
-        |> Enum.reduce_while({t, stats}, fn [person, hu], {q, stats} ->
-          case stats[person] do
-            nil ->
-              # まだ未確定
-              {:cont, {[person|q], Map.put(stats, person, hu)}}
-            ^hu ->
-              # 過去の証言と一致
-              {:cont, {q, stats}}
-            hoge ->
-              IO.inspect("expect #{hoge} but #{hu}", label: "#{h} #{person}")
-              # 過去の証言と一致しない(矛盾)
-              {:halt, :contradiction}
-          end
-        end)
+          _ ->
+            # 証言と一致しない(矛盾)
+            {:halt, :contradiction}
+        end
+      end)
 
-      case res do
-        :contradiction -> 0
-        {q, stats} ->
-          judge(q, testimony, stats)
-      end
-    else
-      judge(t, testimony, stats)
+    case res do
+      :contradiction -> :ng
+      :ok -> judge(t, testimony, stats)
     end
   end
 
