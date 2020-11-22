24268882d4e4fc611bb238a9c71a2a1fae4aa930
tamanugi <tamanugi@gmail.com>
2020-11-22 18:05:03 +0900

    e crested ibis vs monster TLE

--- lib/beginner_bootcamp/hard/e_crested_Ibis_vs_monstera.ex
+++ lib/beginner_bootcamp/hard/e_crested_Ibis_vs_monstera.ex
@@ -1,5 +1,7 @@
 defmodule CrestedIbisMonster.Main do
 
+  @inf 1000_000_000
+
   def read_array() do
     IO.read(:line) |> String.trim() |> String.split(" ") |> Enum.map(&String.to_integer/1)
   end
@@ -13,13 +15,31 @@ defmodule CrestedIbisMonster.Main do
 
   def main() do
     [h, _] = read_array()
-    abs = read_multi_array()
+    abs = read_multi_array() |> Enum.sort_by(fn [_, b] -> b end)
 
-    # MP効率がいい魔法
-    altema = abs |> Enum.max_by(fn [a, b] -> a/b end)
+    # 一番消費MPが少ないものよりHPが少ない場合は明らかにそれを使えばok
+    [[a, b] | _] = abs
+    if h <= a do
+      b
+    else
+      dp(0..h, abs, %{0 => 0})
+      |> Map.get(h)
+    end
+    |> IO.puts()
+  end
 
-    IO.inspect(altema)
+  def dp(h..h, _, dp), do: dp
+  def dp(i..h, abs, dp) do
 
+    dp =
+      abs
+      |> Enum.reduce(dp, fn [a, b], dp ->
+        next_index = min(i + a, h)
+        v = min(Map.get(dp, next_index, @inf), Map.get(dp, i, @inf) + b)
+        Map.put(dp, next_index, v)
+      end)
+
+    dp(i+1..h, abs, dp)
   end
 
 end
