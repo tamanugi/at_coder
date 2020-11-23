cc0fb2d4342ab6766f71a3f750ca61ee048f1e76
tamanugi <tamanugi@gmail.com>
2020-11-23 17:45:38 +0900

    ABC184 d AC

--- lib/abc184/d.ex
+++ lib/abc184/d.ex
@@ -13,12 +13,15 @@ defmodule Abc184.D.Main do
     (for i <- 99..a, j <- 99..b, k <- 99..c, i + j + k > 0, do: {i,j,k})
     |> Enum.reduce(%{}, fn {i, j, k}, dp ->
       s = i + j + k
-      ii = Map.get(dp, {i + 1, j, k}, 0) * i/s
-      jj = Map.get(dp, {i, j + 1, k}, 0) * j/s
-      kk = Map.get(dp, {i, j, k + 1}, 0) * k/s
+      ii = Map.get(dp, key(i + 1, j, k), 0) * i/s
+      jj = Map.get(dp, key(i, j + 1, k), 0) * j/s
+      kk = Map.get(dp, key(i, j, k + 1), 0) * k/s
 
-      Map.put(dp, {i, j, k}, ii + jj + kk + 1)
+      Map.put(dp, key(i, j, k), ii + jj + kk + 1)
     end)
+    |> Map.get(key(a, b, c))
+    |> IO.puts()
   end
 
+  def key(i, j, k), do: i * 1000 * 1000 + j * 1000 + k
 end
