aeefd9d63b781b72a05165bfacf8d25036720f39
tamanugi <tamanugi@gmail.com>
2020-11-24 21:51:43 +0900

    d knight ac

--- lib/beginner_bootcamp/hard/d_knight.ex
+++ lib/beginner_bootcamp/hard/d_knight.ex
@@ -8,46 +8,44 @@ defmodule Knight.Main do
   def main() do
     [x, y] = read_array()
 
-    combi(x+y, x)
-    |> mod(@mod)
-    |> IO.inspect()
+    s = x + y
+
+    with 0 <- rem(s, 3),
+      n <- div(s, 3),
+      k when k >= 0 <- div(n, 2) - (div(s, 2) - min(x, y)) do
+
+      combi(n, k)
+      |> mod(@mod)
+    else
+      _ -> 0
+    end
+    |> IO.puts()
+
   end
 
   def combi(n, a) do
     x = fac(n, a)
     y = fac(a) |> pow(@mod - 2)
-    rem(x * y, @mod)
+    mod(x * y, @mod)
   end
   def pow(_, 0), do: 1
-  def pow(m, 2), do: rem(m * m, @mod)
+  def pow(m, 2), do: mod(m * m, @mod)
   def pow(m, n) do
     case div_and_rem(n, 2) do
       {a, 0} -> pow(m, a) |> pow(2)
       {a, 1} -> (pow(m, a) |> pow(2)) * m
     end
-    |> rem(@mod)
+    |> mod(@mod)
   end
   def div_and_rem(m, n) do
-    {div(m, n), rem(m, n)}
-  end
-
-  def combination(_, 0), do: [[]]
-  def combination([], _), do: []
-
-  def combination([x | xs], n) do
-    for(y <- combination(xs, n - 1), do: [x | y]) ++ combination(xs, n)
-  end
-    def permutation(_, 0), do: [[]]
-
-  def permutation(list, n) do
-    for x <- list, rest <- permutation(list -- [x], n - 1), do: [x | rest]
+    {div(m, n), mod(m, n)}
   end
 
   def fac(n), do: fac(n, n, 1)
   def fac(n, i), do: fac(n, i, 1)
 	defp fac(0, _, cur), do: cur
 	defp fac(_, 0, cur), do: cur
-  defp fac(n, i, cur) when n > 0, do: fac(n - 1, i - 1, rem(cur * n, @mod))
+  defp fac(n, i, cur) when n > 0, do: fac(n - 1, i - 1, mod(cur * n, @mod))
 
   def mod(x, y) when x > 0, do: rem(x, y)
   def mod(x, y) when x < 0, do: mod(y + x, y)
